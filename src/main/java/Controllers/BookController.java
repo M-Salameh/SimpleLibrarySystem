bf7ad73eca46d1eaef1f31298b8d52175c9796ef
mohammad.salama <mohammad.salama@hiast.edu.sy>
2024-10-31 14:09:37 +0300

    JUnit tests Completed with in memory temporary mock database (h2) and simple logging added in app.log to view stacks and tracing

--- src/main/java/Controllers/BookController.java
+++ src/main/java/Controllers/BookController.java
@@ -6,13 +6,15 @@ import IRopositries.IBookRepository;
 import Services.BookService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import org.springframework.validation.annotation.Validated;
 
 import java.util.List;
-import java.util.Optional;
+
 
 
 @RestController
@@ -27,12 +29,14 @@ public class BookController {
 
     // Retrieve all books
     @GetMapping
+    //@Cacheable(value = "All-Books")
     public List<Book> getAllBooks() {
         return bookRepository.findAll();
     }
 
     // Get a book by its ISBN
     @GetMapping("/isbn/{isbn}")
+    //@Cacheable(value = "Books-by-ISBN" , key = "#isbn")
     public ResponseEntity<Book> getBookByIsbn(@PathVariable String isbn)
     {
         Book book = bookService.findByIsbn(isbn);
@@ -41,6 +45,7 @@ public class BookController {
 
     // Retrieve a specific book by ID
     @GetMapping("/{id}")
+    //@Cacheable(value = "Books-by-ID" , key = "#id")
     public ResponseEntity<Book> getBookById(@PathVariable Long id)
     {
         Book book = bookService.getBookById(id);
@@ -49,12 +54,14 @@ public class BookController {
 
     // Add a new book
     @PostMapping
+    //@CacheEvict(value = "All-Books" , allEntries = true)
     public Book createBook(@Valid @RequestBody Book book) {
         return bookService.createBook(book);
     }
 
     // Update an existing book
     @PutMapping("/{id}")
+    //@CacheEvict(value = {"All-Books" , "Books-by-ISBN"} , allEntries = true)
     public ResponseEntity<Book> updateBook(@PathVariable Long id, @Valid @RequestBody Book bookDetails) {
 
         Book book = bookService.updateBook(id , bookDetails);
@@ -63,6 +70,7 @@ public class BookController {
 
     // Remove a book
     @DeleteMapping("/{id}")
+    //@CacheEvict(value = {"All-Books" , "Books-by-ID" , "Books-by-ISBN"} , allEntries = true)
     public ResponseEntity<Void> deleteBook(@PathVariable Long id) {
         bookService.deleteBookById(id);
         return ResponseEntity.noContent().build();
--- src/main/java/Controllers/PatronController.java
+++ src/main/java/Controllers/PatronController.java
@@ -6,6 +6,8 @@ import IRopositries.IPatronRepository;
 import Services.PatronService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,12 +26,14 @@ public class PatronController {
 
     // Retrieve all patrons
     @GetMapping
+    //@Cacheable(value = "All-Patrons")
     public List<Patron> getAllPatrons() {
         return patronRepository.findAll();
     }
 
     // Retrieve a specific patron by ID
     @GetMapping("/{id}")
+   // @Cacheable(value = "Patrons-by-ID" , key = "#id")
     public ResponseEntity<Patron> getPatronById(@PathVariable Long id) {
 
         Patron patron = patronService.getPatronById(id);
@@ -40,6 +44,7 @@ public class PatronController {
 
     // Add a new patron
     @PostMapping
+    //@CacheEvict(value = "All-Patrons")
     public ResponseEntity<Patron> createPatron(@Valid @RequestBody Patron patron) {
         Patron patron1 = patronService.createPatron(patron);
         return ResponseEntity.ok(patron1);
@@ -47,6 +52,7 @@ public class PatronController {
 
     // Update an existing patron
     @PutMapping("/{id}")
+    //@CacheEvict(value = "All-Patrons")
     public ResponseEntity<Patron> updatePatron(@PathVariable Long id, @Valid @RequestBody Patron patronDetails)
     {
         Patron patron = patronService.updatePatron(id , patronDetails);
@@ -55,6 +61,7 @@ public class PatronController {
 
     // Remove a patron
     @DeleteMapping("/{id}")
+    //@CacheEvict(value = {"All-Patrons"  , "Patrons-by-ID"} , allEntries = true)
     public ResponseEntity<Void> deletePatron(@PathVariable Long id) {
         patronService.deletePatron(id);
 
